ec7746940838de1adb32e1ca7979c7ee69c780e8
rtperezyera <A01374866@itesm.mx>
2019-02-17 18:49:28 -0600

    Initial commit

---
+++ desktop/src/mx/itesm/equipo5/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package mx.itesm.equipo5.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import mx.itesm.equipo5.PantallaCargando;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PantallaCargando(), config);
+	}
+}
