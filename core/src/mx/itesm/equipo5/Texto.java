72676ff24361835e5b6328f62fc6c62054b27869
rtperezyera <A01374866@itesm.mx>
2019-03-09 22:30:49 -0600

    Created Text class
    Clase texto creada. Esqueleto de la clase creado. Revisar con equipo fuente y color.
    Checar abributo bounds para que todo se quede en la pantalla.

---
+++ core/src/mx/itesm/equipo5/Texto.java
@@ -0,0 +1,28 @@
+package mx.itesm.equipo5;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class Texto {
+    /*
+    private BitmapFont font;
+
+    public Texto() {
+        // load font
+        font = new BitmapFont(Gdx.files.internal(".fnt"));  // font name
+    }
+
+    public void showMessage(SpriteBatch batch, String mensaje, float x, float y) {
+        GlyphLayout glyph = new GlyphLayout();
+        glyph.setText(font, mensaje);
+        float anchoTexto = glyph.width;
+        font.draw(batch, glyph, x-anchoTexto/2, y); // centrado en x respecto al texto, NO la screen
+
+    }
+
+    // text will be shown (drawn) within the batch.begin and .end lines,
+    // in the class containing those
+    */
+}
