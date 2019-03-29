46e3ae455e927de58f9bdc3e9815a78210493a46
rtperezyera <A01374866@itesm.mx>
2019-03-29 13:55:47 -0600

    Changed ancho, alto --> ANCHO, ALTO
    plus other changes in button construction.
    Should have committed this as i went along

--- core/src/mx/itesm/equipo5/Pantallas/PantallaAjustes.java
+++ core/src/mx/itesm/equipo5/Pantallas/PantallaAjustes.java
@@ -1,6 +1,11 @@
 package mx.itesm.equipo5.Pantallas;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
 
 import mx.itesm.equipo5.Pantalla;
 import mx.itesm.equipo5.Virusito;
@@ -12,11 +17,27 @@ class PantallaAjustes extends Pantalla {
 
     @Override
     public void show() {
+        camara = new OrthographicCamera(PantallaCargando.ANCHO, PantallaCargando.ALTO);
+        camara.position.set(PantallaCargando.ANCHO/2, PantallaCargando.ALTO/2,0);
+        camara.update();
+        // Vista
+        vista = new StretchViewport(PantallaCargando.ANCHO, PantallaCargando.ALTO,camara);
+        batch = new SpriteBatch();
 
     }
 
     @Override
     public void render(float delta) {
+        Gdx.gl.glClearColor(0,0,0,1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        batch.setProjectionMatrix(camara.combined);        // para escalar
+
+
+        batch.begin();
+
+
+        batch.end();
 
     }
 
@@ -37,7 +58,7 @@ class PantallaAjustes extends Pantalla {
 
     @Override
     public void hide() {
-
+        batch.dispose();
     }
 
     @Override
--- core/src/mx/itesm/equipo5/Pantallas/PantallaMenu.java
+++ core/src/mx/itesm/equipo5/Pantallas/PantallaMenu.java
@@ -40,49 +40,15 @@ public class PantallaMenu extends Pantalla {
     private void crearBotones() {
         escenaMenu = new Stage(vista);
 
-        //creacion, posicion y que hace el boton
+
+
         playBoton = new Boton("Botones/Play_Bttn.png").getiButton();
-        playBoton.setPosition(PantallaCargando.ancho/2-playBoton.getWidth()/2,
-                PantallaCargando.alto/3f-playBoton.getHeight()/4f);
-        escenaMenu.addActor(playBoton);
-        playBoton.addListener(new ClickListener(){
+        playBoton.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
                 super.clicked(event, x, y);
+                // Responder al evento del boton
                 juego.setScreen(new Nivel(juego));
-            }
-        });
-
-        aboutBoton = new Boton("Botones/Info_Bttn.png").getiButton();
-        aboutBoton.setPosition(PantallaCargando.ancho*(3/8f)-aboutBoton.getWidth()/2,0);
-        escenaMenu.addActor(aboutBoton);
-        aboutBoton.addListener(new ClickListener(){
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                super.clicked(event, x, y);
-                juego.setScreen(new PantallaAbout(juego));
-            }
-        });
-
-        confBoton = new Boton("Botones/Engrane_Bttn.png").getiButton();
-        confBoton.setPosition(PantallaCargando.ancho*(4/8f)-confBoton.getWidth()/2,0);
-        escenaMenu.addActor(confBoton);
-        confBoton.addListener(new ClickListener(){
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                super.clicked(event, x, y);
-                juego.setScreen(new PantallaAjustes(juego));
-            }
-        });
-
-        helpBoton = new Boton("Botones/Help_Bttn.png").getiButton();
-        helpBoton.setPosition(PantallaCargando.ancho*(5/8f)-helpBoton.getWidth()/2,0);
-        escenaMenu.addActor(helpBoton);
-        helpBoton.addListener(new ClickListener(){
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                super.clicked(event, x, y);
-                juego.setScreen(new PantallaAyuda(juego));
             }
         });
 
