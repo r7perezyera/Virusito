2948197f6a5b6d6d983cf96bec77373b0bd3e584
rtperezyera <A01374866@itesm.mx>
2019-03-31 17:02:35 -0600

    Funcionalidad HelpScreen

--- core/src/mx/itesm/equipo5/Pantallas/HelpScreen.java
+++ core/src/mx/itesm/equipo5/Pantallas/HelpScreen.java
@@ -1,20 +1,64 @@
 package mx.itesm.equipo5.Pantallas;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+
+import mx.itesm.equipo5.Button;
 import mx.itesm.equipo5.MasterScreen;
 import mx.itesm.equipo5.Virusito;
 
 class HelpScreen extends MasterScreen {
+
+    private Texture background;
+
+    private Stage helpStage;
+
+    private ImageButton homeButton;
+
     public HelpScreen(Virusito juego) {
         super(juego);
     }
 
     @Override
     public void show() {
+        helpStage = new Stage(view);
 
+        background = new Texture("Pantallas/PantallaHowTo.jpg");
+
+        createButtons();
+
+        Gdx.input.setInputProcessor(helpStage);
+        Gdx.input.setCatchBackKey(false);
+    }
+
+    private void createButtons() {
+        homeButton = new Button("Botones/Home_Bttn.png").getiButton();
+        homeButton.setPosition(15, (MasterScreen.HEIGHT-homeButton.getHeight())-10);
+        helpStage.addActor(homeButton);
+        homeButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                super.clicked(event, x, y);
+                // Responder al evento del boton
+                game.setScreen(new MenuScreen(game));
+            }
+        });
     }
 
     @Override
     public void render(float delta) {
+        eraseScreen();
+
+        batch.begin();
+        batch.draw(background, 0, 0);
+
+        batch.end();
+
+        helpStage.draw();
 
     }
 
