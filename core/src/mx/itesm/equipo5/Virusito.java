c792964e10c2e69db90186258dec816d729a0762
rtperezyera <A01374866@itesm.mx>
2019-04-20 17:06:24 -0500

    Mounting audio manager in Virusito.java
    TESTING

--- core/src/mx/itesm/equipo5/Virusito.java
+++ core/src/mx/itesm/equipo5/Virusito.java
@@ -1,15 +1,42 @@
 package mx.itesm.equipo5;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.assets.AssetManager;
 
 import mx.itesm.equipo5.Pantallas.LoadingScreen;
 
 public class Virusito extends Game {
 
+    // Asset Manager
+    /*
+    will try initially to manage music and SFX
+    we could eventually turn it into THE only manager for the whole game,
+    i.e. for all the resources that require the asset manager
+     */
+    private final AssetManager audioManager;
+
+    public Virusito() {
+        audioManager = new AssetManager();
+    }
+
     @Override
     public void create(){
+        // por ahora NO necesitamos prepararlo para que cargue mapas
+        //assetManager.setLoader(TiledMap.class, new TmxMapLoader(new InternalFileHandleResolver()));
 
-        setScreen(new LoadingScreen(this));
+        // Pone la pantalla inicial
+        setScreen(new LoadingScreen(this));     // original, keep this
+    }
+
+    // Para que las otras pantallas usen el audioManager
+    public AssetManager getAudioManager() {
+        return audioManager;
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+        audioManager.clear();
     }
 
 }
