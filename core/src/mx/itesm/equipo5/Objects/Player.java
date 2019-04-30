70db4a98adab3767a8ef6023bd136938b41c587a
FanchiniRudolf <43999588+FanchiniRudolf@users.noreply.github.com>
2019-04-30 17:34:09 -0500

    animacion

--- core/src/mx/itesm/equipo5/Objects/Player.java
+++ core/src/mx/itesm/equipo5/Objects/Player.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
+import com.badlogic.gdx.utils.Array;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -24,26 +25,37 @@ public class Player extends Entity {
     private Animation animation;
     private float animationTimer;
 
-    public Player(float x, float y, float health) {
 
+    public Player(float x, float y, float health) {
+        width = 50;
+        height = 57;
         this.health = health;
         this.speed = 7;
         //Load texture
-        texture = new Texture("Personajes/Main_Down.png");
+        texture = new Texture("Animaciones/Principal_Animacion.png");
 
-        animation = new Animation(0.15f,texture);
+        TextureRegion region = new TextureRegion(texture);
+        TextureRegion[][] texturasPersonajes = region.split(width,height);
+
+        Array<TextureRegion> textureRegionArray = new Array(texturasPersonajes[0]);
+
+        animation = new Animation(0.15f,textureRegionArray);
         animation.setPlayMode(Animation.PlayMode.LOOP);
         animationTimer = 0;
 
         //Sprites
-        sprite = new Sprite(texture);
+        sprite = new Sprite(texturasPersonajes[0][0]);
         sprite.setPosition(x,y);
 
-        rectangle.set(x,y,texture.getWidth(),texture.getHeight());
+        rectangle.set(x,y,width,height);
+
+
     }
 
     public void render(SpriteBatch batch) {
-        batch.draw(texture, sprite.getX(), sprite.getY());
+        animationTimer += Gdx.graphics.getDeltaTime();
+        TextureRegion region = (TextureRegion) animation.getKeyFrame(animationTimer);
+        batch.draw(region, sprite.getX(), sprite.getY());
     }
 
 }
