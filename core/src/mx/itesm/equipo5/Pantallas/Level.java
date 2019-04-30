15df9bb6e1712103f4b402abd594912cd26a6ff6
rtperezyera <A01374866@itesm.mx>
2019-04-30 14:24:09 -0500

    Implementing prefs in Level

--- core/src/mx/itesm/equipo5/Pantallas/Level.java
+++ core/src/mx/itesm/equipo5/Pantallas/Level.java
@@ -1,6 +1,7 @@
 package mx.itesm.equipo5.Pantallas;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Preferences;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.assets.loaders.resolvers.InternalFileHandleResolver;
 import com.badlogic.gdx.audio.Music;
@@ -73,6 +74,13 @@ class Level extends MasterScreen {
     private Array<Rectangle> doors;
     private Array<Rectangle> enemyRect;
     private Music music;
+
+    // Users preferences
+    private Preferences lvlPrefs = Gdx.app.getPreferences("userPrefs");
+    boolean isSoundOn = lvlPrefs.getBoolean("soundOn");
+
+//    private Preferences levelPreferences = Gdx.app.getPreferences("usersPreferences");
+//    boolean musicaActivada = levelPreferences.getBoolean("soundOn");
 
 
     public Level(Virusito juego) {
